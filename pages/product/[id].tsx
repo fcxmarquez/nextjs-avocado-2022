360a3bf6b34efbb217d66555c3c95b430fa3c658
Francisco <francisco.marquez.solt@gmail.com>
2022-01-18 21:23:30 -0600

    Creando y consumiendo nuestra propia API

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    if (id) {
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((response) => response.json())
+        .then(({ data }) => {
+          setProduct(data)
+        })
+    }
+  }, [id])
 
   return (
-    <section>
-      <h1>Página producto: {query.id}</h1>
-    </section>
+    <div>
+      <h1>{product?.name}</h1>{' '}
+      {/* Conditional cause maybe you don't recive any params */}
+    </div>
   )
 }
 
