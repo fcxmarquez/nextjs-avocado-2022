0a3ecf4b99da692171d1c3f12c68c1b00be08ed1
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-08-18 19:33:36 -0500

    Image optimization

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 import { ProductListProps } from './ProductList.interface'
+import Image from 'next/image'
 
 export const ProductList: React.FC<ProductListProps> = ({ products }) => {
-  
   return (
     <Card.Group centered>
       {products.map((product) => (
         <Link href={`/product/${product.id}`} passHref key={product.id}>
           <Card>
-            <Image src={product.image} />
+            <Image src={product.image} width="290" height="290" />
             <Card.Content style={{ color: 'black' }}>
               <h3>{product.name}</h3>
               <p>${product.price}</p>
